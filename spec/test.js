50c24288260c6bffaf85e722cf9ef5fc21ae2a5a
Adam <adam_ali_96@hotmail.co.uk>
2016-12-16 14:08:48 +0000

    started order component and compleated tests

--- spec/test.js
+++ spec/test.js
@@ -5,12 +5,13 @@ var assert = require('chai').assert;
 describe('calculation functions', () => {
   var apple = {id: 'a1', name: 'apple', price: 1.20};
   var asparagus = {id: 'G95', name: 'Asparagus', price: 1.20};
-
+  var banana = {id: 'G94', name: 'banana', price: 0.2};
+  var melon = {id: 'G93', name: 'melon', price: 5.20};
   it('select product adds one item to the array', () => {
     var actual = [];
     calculate.selectProduct(apple, actual);
-    var ordersArrayContent = {id: 'a1', name: 'apple', price: 1.20, totalPrice: '1.20', quantity: 1};
-    const expected = [ordersArrayContent];
+    var ordersArray = {id: 'a1', name: 'apple', price: 1.20, totalPrice: '1.20', quantity: 1};
+    const expected = [ordersArray];
 
     assert.deepEqual(actual, expected);
   });
@@ -19,8 +20,8 @@ describe('calculation functions', () => {
     const actual = [];
     calculate.selectProduct(apple, actual);
     calculate.selectProduct(apple, actual);
-    var ordersArrayContent = {id: 'a1', name: 'apple', price: 1.20, quantity: 2, totalPrice: '2.40'};
-    const expected = [ordersArrayContent];
+    var ordersArray = {id: 'a1', name: 'apple', price: 1.20, quantity: 2, totalPrice: '2.40'};
+    const expected = [ordersArray];
 
     assert.deepEqual(actual, expected);
   });
@@ -28,8 +29,8 @@ describe('calculation functions', () => {
   it('reducer should give normal price when 1 Asparagus is  selected', () => {
     const actual = [];
     calculate.selectProduct(asparagus, actual);
-    var ordersArrayContent = {id: 'G95', name: 'Asparagus', quantity: 1, price: 1.20, totalPrice: '1.20'};
-    const expected = [ordersArrayContent];
+    var ordersArray = {id: 'G95', name: 'Asparagus', quantity: 1, price: 1.20, totalPrice: '1.20'};
+    const expected = [ordersArray];
 
     assert.deepEqual(actual, expected);
   });
@@ -38,8 +39,42 @@ describe('calculation functions', () => {
     const actual = [];
     calculate.selectProduct(asparagus, actual);
     calculate.selectProduct(asparagus, actual);
-    var ordersArrayContent = {id: 'G95', name: 'Asparagus', price: 1.20, quantity: 2, totalPrice: '1.20', fullPrice: '£2.40'};
-    const expected = [ordersArrayContent];
+    var ordersArray = {id: 'G95', name: 'Asparagus', price: 1.20, quantity: 2, totalPrice: '1.20', fullPrice: '£2.40'};
+    const expected = [ordersArray];
+
+    assert.deepEqual(actual, expected);
+  });
+
+  it('reducer should give one free when 3 Asparagus are  selected', () => {
+    const actual = [];
+    calculate.selectProduct(asparagus, actual);
+    calculate.selectProduct(asparagus, actual);
+    calculate.selectProduct(asparagus, actual);
+    var ordersArray = {id: 'G95', name: 'Asparagus', price: 1.20, quantity: 3, totalPrice: '2.40', fullPrice: '£3.60'};
+    const expected = [ordersArray];
+
+    assert.deepEqual(actual, expected);
+  });
+  it('reducer should add 3 different items to the state when they are  selected', () => {
+    const actual = [];
+    calculate.selectProduct(apple, actual);
+    calculate.selectProduct(banana, actual);
+    calculate.selectProduct(melon, actual);
+    var ordersArray1 = {id: 'a1', name: 'apple', price: 1.20, quantity: 1, totalPrice: '1.20'};
+    var ordersArray2 = {id: 'G94', name: 'banana', price: 0.20, quantity: 1, totalPrice: '0.20'};
+    var ordersArray3 = {id: 'G93', name: 'melon', price: 5.20, quantity: 1, totalPrice: '5.20'};
+
+    const expected = [ordersArray1, ordersArray2, ordersArray3];
+
+    assert.deepEqual(actual, expected);
+  });
+  it('reducer should add 2 different items to the state when 3 items are selected but one is selected twice', () => {
+    const actual = [];
+    calculate.selectProduct(apple, actual);
+    calculate.selectProduct(banana, actual);
+    var ordersArray1 = {id: 'a1', name: 'apple', price: 1.20, quantity: 1, totalPrice: '1.20'};
+    var ordersArray2 = {id: 'G94', name: 'banana', price: 0.20, quantity: 1, totalPrice: '0.20'};
+    const expected = [ordersArray1, ordersArray2];
 
     assert.deepEqual(actual, expected);
   });
--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -2,17 +2,37 @@ import React, { Component } from 'react';
 import './Order.css';
 
 class OrderComponent extends Component {
+  constructor () {
+    super();
+    this.state = {
+      total: 0.00,
+      subtotal: 0.00,
+      discountNote: '',
+      buy1get1free: ''
+    };
+  }
   render () {
     return (
       <div className='Order'>
         <h2 className='Order-title'>Your Order</h2>
+        <table className='table table-bordered'>
+          <tbody id='myTable'>
+            <tr className='titles'>
+              <th>Item</th>
+              <th>Price</th>
+              <th>Quantity</th>
+              <th>Total</th>
+            </tr>
+
+          </tbody>
+        </table>
       </div>
     );
   }
 }
 
 OrderComponent.propTypes = {
-
+  order: React.PropTypes.array
 };
 
 export default OrderComponent;
